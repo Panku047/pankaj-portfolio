93252d38bc0e045fe144e1bb64245d608a1f0a4e
bitbucket ofc <pankajkumar199528@gmail.com>
2024-01-15 01:34:15 +0530

    href added to a tag

--- src/components/header/MyNavbar.jsx
+++ src/components/header/MyNavbar.jsx
@@ -5,12 +5,12 @@ const MyNavbar = ({scrollToSection}) =>{
     return(
         <div className="nav">
             <div class="topnav">
-                <a onClick={() => scrollToSection('about')}>About</a>
-                <a onClick={() => scrollToSection('skills')}>Skills</a>
-                <a href="#contact">Experience</a>
-                <a href="#about">Project</a>
-                <a href="#contact">Interest</a>
-                <a onClick={() => scrollToSection('contact')}>Contact</a>
+                <a href="#about" onClick={() => scrollToSection('about')}>About</a>
+                <a href="#skills" onClick={() => scrollToSection('skills')}>Skills</a>
+                <a href="#experience">Experience</a>
+                <a href="#project">Project</a>
+                <a href="#interest">Interest</a>
+                <a href="#contact" onClick={() => scrollToSection('contact')}>Contact</a>
             </div>
         </div>
     );
